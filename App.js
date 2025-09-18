e10b6f306905ac64c445ee1e5aa73276b9a97f5d
asijaredzic <redzasi2012@gmail.com>
2025-09-18 20:06:23 +0200

    lekcija1

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>My name is Asija Redžić!</Text>
+      <Text>I am 13 years old!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +14,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#f6acacff',
     alignItems: 'center',
     justifyContent: 'center',
   },
 });
+
